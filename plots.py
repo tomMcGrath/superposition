00eea3fc4fcfb3e68af98e951b121818b92f0413
tomMcGrath <thomas.m.mcgrath@gmail.com>
2024-04-06 15:14:36 +0100

    add colorbar to plots.py

--- plots.py
+++ plots.py
@@ -6,5 +6,6 @@ def feature_mat(model):
     W = model.W.to('cpu').detach().numpy()
     feature_mat = W @ W.T
     max_abs = np.max(np.abs(feature_mat))
-    plt.matshow(feature_mat, cmap='RdBu', vmin=-max_abs, vmax=max_abs)
+    p = plt.matshow(feature_mat, cmap='RdBu', vmin=-max_abs, vmax=max_abs)
+    plt.colorbar(p)
     plt.show()
