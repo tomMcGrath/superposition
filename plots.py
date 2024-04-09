1c62571c6179a8c596dd3d287c0cef9a78b1e921
Tom McGrath <thomas.m.mcgrath@gmail.com>
2024-04-09 16:06:08 +0100

    added smoothing to plots.py

--- plots.py
+++ plots.py
@@ -1,8 +1,16 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import pandas as pd
+
+
+def smooth(x, span):
+    """Smooths a time series using pandas EWM."""
+    x = pd.Series(x)
+    return x.ewm(span=span).mean()
 
 
 def feature_mat(model):
+    """Visualises a model's feature matrix."""
     W = model.W.to('cpu').detach().numpy()
     feature_mat = W @ W.T
     max_abs = np.max(np.abs(feature_mat))
