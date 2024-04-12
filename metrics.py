d079677c940a9b614ba3bec65a430de3e87128c4
Tom McGrath <thomas.m.mcgrath@gmail.com>
2024-04-12 10:57:59 -0700

    add l0 and dead neuron count to metrics.py

--- metrics.py
+++ metrics.py
@@ -1,4 +1,5 @@
 import numpy as np
+import torch
 
 
 def get_superpos(W, idx):
@@ -13,3 +14,15 @@ def get_superpos(W, idx):
             sp += np.dot(W[idx], W[i])**2
             
     return sp
+
+
+def mean_l0(x):
+    """Returns mean number of entries of x that are larger than eps."""
+    per_sample_norms = torch.linalg.vector_norm(x, ord=0, dim=1)
+    return torch.mean(per_sample_norms)
+
+
+def dead_neurons(f):
+    """Returns the number of dimensions of f that are always zero."""
+    num_live_neurons = torch.count_nonzero(torch.sum(f, dim=0))
+    return f.shape[0] - num_live_neurons
