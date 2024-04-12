443a5936602f4a969fb978c795a74615fb2daf33
Tom McGrath <thomas.m.mcgrath@gmail.com>
2024-04-12 11:54:14 -0700

    sae training file

---
+++ train_sae.py
@@ -0,0 +1,28 @@
+import config
+import datasets
+import models
+import metrics
+import torch
+import wandb
+import train
+
+
+if __name__ == '__main__':
+    # Get config
+    cfg = config.cfg
+    device = cfg['train_device']
+
+    # Load model
+    model_path = 'models/test.pt'
+    model = torch.load(model_path)
+
+    # Initialise dataset
+    data_cfg = cfg['data_config']
+    n = data_cfg['data_dim']
+    S = data_cfg['p_sparse']
+    raw_dist = datasets.make_uniform_dist(n, device)
+    sparsity_dist = datasets.make_bernoulli_dist(S, n, device=device)
+    dataset = datasets.SparseDataset(raw_dist, sparsity_dist)
+
+    # SAE training
+    sae_train_outs = train.train_sae(cfg, model, dataset)
